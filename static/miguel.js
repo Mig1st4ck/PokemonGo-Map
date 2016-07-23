b3ba7bab733f1e1290e0381d297ae6bae6424aeb
Miguel Couto <jmiguelcouto@gmail.com>
2016-07-23 18:29:38 +0100

    Added Support to see updated list on left panel.

--- static/miguel.js
+++ static/miguel.js
@@ -16,12 +16,11 @@ function setupMiguelPokemon(item) {
     }
     if (show > 0) {
         var disappear_date = new Date(item.disappear_time);
-        console.log(new_id, item.pokemon_name, 
-        pad(disappear_date.getHours()) + ':' + pad(disappear_date.getMinutes()) + ':' + pad(disappear_date.getSeconds())
-        , item.latitude + ',' + item.longitude);
+        var desapear_at = `Disappears at ${pad(disappear_date.getHours())}:${pad(disappear_date.getMinutes())}:${pad(disappear_date.getSeconds())}`;
+        console.log(new_id, item.pokemon_name, desapear_at);
         if (show === 3){
             // todo Change to use the new notifications
-            sendMyNotification('A wild ' + item.pokemon_name + ' appeared!', 'Click to load map', 'static/icons/' + item.pokemon_id + '.png', item);
+            sendMyNotification('A wild ' + item.pokemon_name + ' appeared!', desapear_at, 'static/icons/' + item.pokemon_id + '.png', item);
             // prompt(new_id + ": " + item.pokemon_name + " Founded", item.latitude + ',' + item.longitude);
         }
     } else {
@@ -35,8 +34,8 @@ function setupMiguelPokemon(item) {
             pocos: []
         }
     }
-    if (pokemons[new_id].pocos.indexOf(item.latitude + ',' + item.longitude) < 0){
-        pokemons[new_id].pocos.push(item.latitude + ',' + item.longitude);
+    if (pokemons[new_id].pocos.indexOf(item.encounter_id) < 0){
+        pokemons[new_id].pocos.push(item.encounter_id);
     }
     pokemons[new_id].count = pokemons[new_id].pocos.length;
 
@@ -52,6 +51,10 @@ function sendMyNotification(title, text, icon, item) {
             sound: 'sounds/ding.mp3'
         });
 
+        if(localStorage.playSound === 'true'){
+          window.audio.play();
+        }
+
         notification.onclick = function () {
             notification.close();
             var latlng = new google.maps.LatLng(item.latitude, item.longitude);
@@ -61,8 +64,16 @@ function sendMyNotification(title, text, icon, item) {
 }
 
 function setupLayout() {
-    var ulPocos = $('<ul id="pokemons" class="links">');
-    var navPocos = $('<nav id="nav-pocos"></nav>').append(ulPocos);
+    var ulPocos = $('<ul id="pokemons" class="links">')
+    var navPocos = $('<nav id="nav-pocos"></nav>').css({
+        'zIndex': 10,
+        'position': 'absolute',
+        'background': '#eee',
+        'width': '300px',
+        'top': '50px',
+        'bottom': '0px',
+        'padding': '15px',
+    }).append(ulPocos);
 
     var newA = $('<a>').attr({
         'href': '#nav-pocos'
@@ -73,5 +84,73 @@ function setupLayout() {
     newA.click(function (){
         navPocos.toggleClass('visible');
     });
+
+    google.maps.event.addListener(map, "rightclick", function(event) {
+        var lat = event.latLng.lat();
+        var lng = event.latLng.lng();
+        // populate yor box/field with lat, lng
+        $.post(`next_loc?lat=${lat}&lon=${lng}`);
+        var latlng = new google.maps.LatLng(lat, lng);
+        marker.setVisible(true);
+        marker.setOptions({'opacity': 1});
+        marker.setPosition(latlng);
+    });
 }
-setupLayout();
+function updateList() {
+    var list = $('#pokemons');
+    var lastStamp = new Date().getTime();
+    $.each(pokemons, function(i, item){
+        var div = list.find('.'+i);
+        if (div.length === 0) {
+            var show = parseInt(localStorage.getItem(i + '_' + item.item.pokemon_name));
+            var background = isNaN(show) || show >= 3 ? 'red': '#ccc';
+            div = $('<li>').css({
+                'list-style': 'none',
+                background: background
+            }).append($('<a>')
+                    .attr('href', '#' + i)
+                    .append($('<img>').attr('src', 'static/icons/'+item.item.pokemon_id+'.png'))
+                    .append(i + ' ' + item.item.pokemon_name)
+                    .append($('<span>').css({
+                        'float': 'right',
+                        'margin-top': '7px',
+                        'padding': '0 10px',
+                        'background': '#fff',
+                        'color': '#222',
+                        'border-radius': '10px'
+                    }).addClass('badge ' + i).text(item.count)))
+                    .data(item.item)
+                    .click(function() {
+                        var new_id = pad3($(this).data().pokemon_id);
+                        var rnd = pokemons[new_id].pos++;
+                        if (rnd > pokemons[new_id].pocos.length - 1) {
+                            rnd = 0;
+                        }
+                        var item = pokemons[new_id].pocos[rnd].split(',');
+                        var latlng = new google.maps.LatLng(item[0], item[1]);
+
+                        map.setCenter(latlng);
+                        // console.log("click ", item, latlng);
+                    });
+            list.append(div);
+        } else {
+            $.each(item.pocos, function(i, val){
+                if (!map_pokemons[val]){
+                    var index = item.pocos.indexOf(val);
+                    if (index >= 0) {
+                        item.pocos.splice(index, 1);
+                    }
+                    item.count = item.pocos.length;
+                }
+            });
+            div.text(item.count);
+        }
+    });
+}
+var clear = setInterval(function(){
+    if (window.map && window.marker) {
+        clearInterval(clear);
+        setupLayout();
+    }
+}, 500);
+setInterval(updateList, 5000);
